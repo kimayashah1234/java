970d3dffdf5165ca363895ee0de3de0069ede0ca
kimayashah1234 <70025159+kimayashah1234@users.noreply.github.com>
2020-09-12 17:03:21 +0530

    Update hellow_world.java

--- hellow_world.java
+++ hellow_world.java
@@ -2,7 +2,7 @@
 /** 
  * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
  */
-public class HelloWorld {
+public class hellow_world {
   public static void main(String[] args) {
     System.out.println("Hello, World");
   }
